6238fbb1b4569ee7f2981a31fd8351c2b33a139a
loic-prosi <loicprosi@gmail.com>
2018-12-08 20:21:37 +0100

    <conflict> fixed conflict

--- layouts/main.js
+++ layouts/main.js
@@ -1,7 +1,12 @@
 import Header from "../components/Header";
+import TitleDescription from "../components/TitleDescription";
+import GroundType from "../components/GroundType";
+
 export default ({ children }) => (
   <div>
     <Header />
+    {/*<TitleDescription plantName={"Nom"} plantDesc={"Description"} plantWiki={""} />*/}
+    <GroundType groundTypes={["clay", "sand"]} />
     {children}
   </div>
 );
