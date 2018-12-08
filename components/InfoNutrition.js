7ed2cf3e4dabba684856b35282f7a2d43206cd64
loic-prosi <loicprosi@gmail.com>
2018-12-08 21:20:22 +0100

    <feature> added InfoNutrition component

---
+++ components/InfoNutrition.js
@@ -0,0 +1,37 @@
+const InfoNutrition = props => (
+  <section>
+    <h3>
+      Valeurs nutritionelles
+    </h3>
+    <h5>pour 100 grammes</h5>
+
+    <ul>
+      <li><b>Calories</b></li>
+      <li><b>Lipides</b></li>
+      <li><b>Cholestérol</b></li>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+    </ul>
+
+    <ul>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+    </ul>
+
+    <ul>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+      <li><b>Calories</b></li>
+    </ul>
+
+  </section>
+);
+
+export default InfoNutrition;
