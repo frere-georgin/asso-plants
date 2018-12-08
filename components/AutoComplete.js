1940825a1776d406599c6db125f95c45bc62913a
thibaud frere <tfrere@student.42.fr>
2018-12-08 19:58:24 +0100

    <Feature> Autocomplete

--- components/AutoComplete.js
+++ components/AutoComplete.js
@@ -1,40 +1,8 @@
 import Autosuggest from "react-autosuggest";
 
-const languages = [
-  {
-    name: "carotte",
-    associations: 13
-  },
-  {
-    name: "courgette",
-    associations: 3
-  },
-  {
-    name: "laitue",
-    associations: 3
-  }
-];
-
-// Teach Autosuggest how to calculate suggestions for any given input value.
-const getSuggestions = value => {
-  const inputValue = value.trim().toLowerCase();
-  const inputLength = inputValue.length;
-
-  return inputLength === 0
-    ? []
-    : languages.filter(
-        lang => lang.name.toLowerCase().slice(0, inputLength) === inputValue
-      );
-};
-
 // When suggestion is clicked, Autosuggest needs to populate the input
 // based on the clicked suggestion. Teach Autosuggest how to calculate the
 // input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
-
-// Use your imagination to render suggestions.
-const renderSuggestion = suggestion => <div>{suggestion.name}</div>;
-
 class AutoComplete extends React.Component {
   constructor() {
     super();
@@ -56,11 +24,27 @@ class AutoComplete extends React.Component {
     });
   };
 
+  getSuggestions = value => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    return inputLength === 0
+      ? []
+      : this.props.data.filter(
+          lang => lang.name.toLowerCase().slice(0, inputLength) === inputValue
+        );
+  };
+
+  getSuggestionValue = suggestion => suggestion.name;
+
+  // Use your imagination to render suggestions.
+  renderSuggestion = suggestion => <div>{suggestion.name}</div>;
+
   // Autosuggest will call this function every time you need to update suggestions.
   // You already implemented this logic above, so just use it.
   onSuggestionsFetchRequested = ({ value }) => {
     this.setState({
-      suggestions: getSuggestions(value)
+      suggestions: this.getSuggestions(value)
     });
   };
 
@@ -88,11 +72,11 @@ class AutoComplete extends React.Component {
     return (
       <div className="auto-complete">
         <Autosuggest
-          suggestions={data}
+          suggestions={suggestions}
           onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
           onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
+          getSuggestionValue={this.getSuggestionValue}
+          renderSuggestion={this.renderSuggestion}
           inputProps={inputProps}
         />
       </div>
