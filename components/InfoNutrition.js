23bede4313a249a039587cb913623eb741b0fc7f
thibaud frere <tfrere@student.42.fr>
2018-12-09 11:26:40 +0100

    <Style> Add icon font and finish infonutrition

--- components/InfoNutrition.js
+++ components/InfoNutrition.js
@@ -1,36 +1,69 @@
 const InfoNutrition = props => (
   <section>
-    <h3>
-      Valeurs nutritionelles
-    </h3>
+    <h3>Valeurs nutritionelles</h3>
     <h5>pour 100 grammes</h5>
 
     <ul>
-      <li><b>Calories</b></li>
-      <li><b>Lipides</b></li>
-      <li><b>Cholestérol</b></li>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
+      <li>
+        <b>Calories</b> 15
+      </li>
+      <li>
+        <b>Lipides</b> 0.2mg
+      </li>
+      <li>
+        <b>Cholestérol</b> Omg
+      </li>
+      <li>
+        <b>Sodium</b> 28mg
+      </li>
+      <li>
+        <b>Potassium</b> 194mg
+      </li>
+      <li>
+        <b>Glucides</b> 2.9g
+      </li>
+      <li>
+        {" "}
+        <b>Fibres alimentaires</b> 1.3mg
+      </li>
+      <li>
+        {" "}
+        <b>Sucres</b> 0.8mg
+      </li>
+      <li>
+        <b>Protéines</b> 1.4mg
+      </li>
     </ul>
 
     <ul>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
+      <li>
+        <b>Vitamine A</b> 7 405 IU
+      </li>
+      <li>
+        <b>Calcium</b> 36mg
+      </li>
+      <li>
+        <b>Vitamine D</b> 0 IU{" "}
+      </li>
+      <li>
+        <b>Vitamine B₁₂</b> 0 µg
+      </li>
     </ul>
 
     <ul>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
-      <li><b>Calories</b></li>
+      <li>
+        <b>Vitamine C</b> 9.2mg
+      </li>
+      <li>
+        <b>Fer</b> 0.9mg
+      </li>
+      <li>
+        <b>Vitamine B6</b> 0.1mg
+      </li>
+      <li>
+        <b>Magnésium</b> 13mg
+      </li>
     </ul>
-
   </section>
 );
 
