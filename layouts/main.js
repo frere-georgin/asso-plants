b43b7243c895830dcd1c65126deddbb9c2d26bc5
WemiceD <georgin.aymeric@gmail.com>
2018-12-09 18:27:42 +0100

    <Refactor> use associations.json

--- layouts/main.js
+++ layouts/main.js
@@ -1,12 +1,39 @@
+import { Component } from "react";
 import Nav from "../components/Nav";
 import AutoComplete from "../components/AutoComplete";
 import "../css/main.scss";
-import associations from "../static/association.js";
+// import associations from "../static/association.json";
 
-export default ({ children }) => (
-  <React.Fragment>
-    <Nav />
-    <AutoComplete data={associations} />
-    {children}
-  </React.Fragment>
-);
+class Layout extends Component {
+  state = {
+    associations: []
+  };
+
+  async componentDidMount() {
+    const associations = await import("../static/association.json");
+    this.setState({ associations: associations.plants });
+  }
+
+  render() {
+    const { associations } = this.state;
+    if (associations) {
+      return (
+        <React.Fragment>
+          <Nav />
+          <AutoComplete data={associations} />
+          {this.props.children}
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+Layout.getInitialProps = async ({ req }) => {
+  const res = await import("../static/association.json");
+  // const json = await res.json();
+  //console.log("JSON", res);
+  console.log("GET initially PROPS");
+  return { associations: res.plants };
+};
+
+export default Layout;
